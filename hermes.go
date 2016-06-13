64c7de4edecfb52ec0f1e765e1e7464dbcc94de8
Samuel Doiron <sam.doiron96@gmail.com>
2016-06-12 21:45:05 -0300

    Remove binary, extract web server

--- hermes.go
+++ hermes.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
-
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		reader, err := os.Open("index.html")
-		if err != nil {
-			w.Write([]byte("error reading index.html!!!"))
-		}
-
-		io.Copy(w, reader)
-	})
-
-	fmt.Println("Listening on port :8080")
-	http.ListenAndServe(":8080", nil)
+	RunWebServer()
 }
---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// RunServer starts a web server on port 8080, which serves the
+// Hermes web interface
+func RunWebServer() {
+	fs := http.FileServer(http.Dir("static"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		reader, err := os.Open("index.html")
+		if err != nil {
+			w.Write([]byte("error reading index.html!!!"))
+		}
+
+		io.Copy(w, reader)
+	})
+
+	fmt.Println("Listening on port :8080")
+	http.ListenAndServe(":8080", nil)
+}
